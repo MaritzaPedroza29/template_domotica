85742eed4cb76528b77a8ea846ccffc1c76d3a20
MaritzaPedroza29 <maritzapedrozacharri@gmail.com>
2023-10-18 18:28:07 -0500

    cambios

--- src/components/AppSidebar.js
+++ src/components/AppSidebar.js
@@ -3,8 +3,7 @@ import { useSelector } from 'react-redux'
 import { CSidebar, CSidebarBrand, CSidebarNav } from '@coreui/react'
 import { CImage } from '@coreui/react'
 import logo from '../assets/images/smartfesc.jpg'
-import { logoNegative } from '../assets/brand/logo-negative'
-import { sygnet } from '../assets/brand/sygnet'
+import "../assets/css/index.css"
 import { AppSidebarNav } from './sidebar/AppSidebarNav'
 import imagen from "../assets/images/smartfesc.jpg"
 
@@ -14,13 +13,18 @@ import items from '../utils/sidebarnav'
 
 const AppSidebar = () => {
   const sidebarShow = useSelector((state) => state.sidebarShow)
+  const customStyles = {
+    backgroundColor: "#000000",
+    padding: '10px',
+
+  };
 
   return (
     <CSidebar
       position="fixed"
       visible={sidebarShow}
     >
-      <CSidebarBrand className="d-none d-md-flex" to="/">
+      <CSidebarBrand className="d-none d-md-flex" to="/" style={customStyles}>
         <CImage rounded src={logo} width={100} height={50} />
       </CSidebarBrand>
       <CSidebarNav>
